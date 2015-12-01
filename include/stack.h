089a3c805d142bf191975aa73bf8e979639b1475
Alexander Khlybov <alekshlybov23@gmail.com>
2015-12-01 20:10:26 +0300

    Stack implementation

--- include/stack.h
+++ include/stack.h
@@ -2,7 +2,25 @@
 #define _STACK_H_
 #include "list.h"
 
+class Stack {
+public:
+	Stack(void);
+	Stack(const Stack*);
+	~Stack(void);
 
+	// Проверка на полноту.
+	int isFull(void) const;
 
+	// Проверка на пустоту.
+	int isEmpty(void) const;
+
+	// Положить элемент в стек.
+	void push(const KeyType&);
+	
+	// Взять элемент из стека.
+	KeyType pop(void);
+private:
+	List* list_;
+};
 
 #endif
