c8ba043f66e5ae30a5196c5a4a004e5abf838b64
Alexander Khlybov <alekshlybov23@gmail.com>
2015-12-24 01:17:54 +0300

    Added new method in Postfix/

--- include/postfix.h
+++ include/postfix.h
@@ -1,35 +1,17 @@
 #ifndef _POSTFIX_H_
 #define _POSTFIX_H_
 #include "stack.hpp"
+#include <map>
 
 #define _SPACE_ ' '
 
 typedef double VariableType;
 
 class Postfix {
-private:
-	Stack<char>* res_;
-	Stack<char>* operator_;
-	Stack<VariableType>* expResult_;
-	string str_;
-    string resStr_;
-	int op;
-
 protected:
-	int rightBr(void);
-	void decreasePriority(int);
-	int  priorietyOperator(const char)const;
-	int  isOperator(const char)const;
-	int  isOperand(const char)const;
-	int checkingLine(void)const;
+	void checkBrackets(const string&)const;
 
 public:
-	Postfix(void);
-	~Postfix(void);
-
-	void procStr(void);
-	int setString(const string&);
-	void printPostfix(void);
-	VariableType calculate(void);
+	string rewriteLineFromInfixToPostfix(const string&);
 };
 #endif
