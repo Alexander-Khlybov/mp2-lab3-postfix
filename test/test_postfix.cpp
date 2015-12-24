0f7731dc0f645ba3fa5da87af20f827c8e6cf3ad
Alexander Khlybov <alekshlybov23@gmail.com>
2015-12-24 12:34:41 +0300

    tests for postfix.

--- test/test_postfix.cpp
+++ test/test_postfix.cpp
@@ -2,3 +2,25 @@
 
 #include <gtest.h>
 
+TEST(Postfix, rewriteLineFromInfixToPostfix_works_properly){
+	string str = "a*(b+c-d)+(d/(f-c)-(k+l)/s)";
+	string str1 = "abcd-+*dfc-/kl+s/-+";
+	Postfix p;
+	EXPECT_EQ(str1, p.rewriteLineFromInfixToPostfix(str));
+}
+
+TEST(Postfix, calculate){
+	Postfix p;
+    string str = "a*(b+c-d)+(d/(f-c)-(k+l)/s)";
+
+    map<char, VariableType> m;
+    m['a'] = 1;
+    m['b'] = 5;
+    m['c'] = 3;
+    m['d'] = 4;
+    m['f'] = 5;
+    m['k'] = 10;
+    m['l'] = 15;
+    m['s'] = 5;
+    EXPECT_EQ(1, p.calculate(p.rewriteLineFromInfixToPostfix(str), m));
+}
